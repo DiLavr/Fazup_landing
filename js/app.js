5d73aa576fc8556fd0ab390a21e8ce978823d889
DiLavr <42397553+DiLavr@users.noreply.github.com>
2019-11-22 00:15:01 -0500

    conact form

--- js/app.js
+++ js/app.js
@@ -1,102 +1,58 @@
-// show the card by
-
-(function ()
+function validateForm()
 {
-    const cartInfo = document.getElementById('cart-info');
-    const cart = document.getElementById('cart');
-    cartInfo.addEventListener('click', function ()
-    {
-        // The toggle() method toggles between hide() and show() for the selected elements.
-        cart.classList.toggle("show-cart");
-    });
-})();
-
-// add items to the cart
-
-(function ()
-{
-    // cartBtn - cart bitton
-    const cartBtn = document.querySelectorAll(".store-item-icon");
-    cartBtn.forEach(function (btn)
-    {
-        // targeting the exact button
-        btn.addEventListener('click', function (event)
-        {
-            //we are targetiing specificly on Icon cart, not the whole element
-            if (event.target.classList.contains("store-item-icon")) {
-
-
-
-
-
-
-                // let name = event.target.parentElement.parentElement.nextElementSibling.
-                //     children[0].children[0].textContent;
-
-
-                let name = document.getElementById("elementName").textContent;
-
-                console.log(name);
-
-                let price = event.target.parentElement.parentElement.nextElementSibling.
-                    children[0].children[1].textContent;
-                // take off $ and space in price
-                let finalPrice = price.slice(1).trim();
-                item.price = finalPrice;
-
-                const cartItem = document.createElement("div");
-                cartItem.classList.add("cart-item", "d-flex", "justify-content-between",
-                    "text-capitalize", "my-3");
-
-                cartItem.innerHTML = `
-                <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
-            <div class="cart-item-text">
-                <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
-                <span>$</span>
-                <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
-            </div>
-            <a href="#" id='cart-item-remove' class="cart-item-remove">
-            <i class="fas fa-trash"></i>
-            </a>
-            </div>
-                `;
-                //select cart
-                const cart = document.getElementById('cart');
-                const total = document.querySelector('.cart-total-container');
-                cart.insertBefore(cartItem, total);
-                alert(
-                    event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent +
-                    " added in the cart");
-                //showTotals
-                showTotal();
-
-            }
-        });
-    });
-
-    //showTotals
-    function showTotal()
-    {
-        // creating an array of all prices, then loop the prices
-        const total = [];
-        const items = document.querySelectorAll(".cart-item-price");
-
-        items.forEach(function (item)
-        {
-            total.push(parseFloat(item.textContent));
-        });
-
-        const totalMoney = total.reduce(function (total, item)
-        {
-            total += item;
-            return total;
-        }, 0);
-        const finalMoney = totalMoney.toFixed(2);
-
-        //insert values
-        document.getElementById("cart-total").textContent = finalMoney;
-        document.querySelector("item-total").textContent = finalMoney;
-        document.getElementById("cart-count").textContent = total.length;
-
+    var x =  document.getElementById('name').value;
+    if (x == "") {
+        document.getElementById('status').innerHTML = "Name cannot be empty";
+        return false;
     }
-})();
+    x =  document.getElementById('email').value;
+    if (x == "") {
+        document.getElementById('status').innerHTML = "Email cannot be empty";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(x)){
+            document.getElementById('status').innerHTML = "Email format invalid";
+            return false;
+        }
+    }
+    x =  document.getElementById('subject').value;
+    if (x == "") {
+        document.getElementById('status').innerHTML = "Subject cannot be empty";
+        return false;
+    }
+    x =  document.getElementById('message').value;
+    if (x == "") {
+        document.getElementById('status').innerHTML = "Message cannot be empty";
+        return false;
+    }
+    //get input field values data to be sent to server
+    document.getElementById('status').innerHTML = "Sending...";
+    formData = {
+        'name': $('input[name=name]').val(),
+        'email': $('input[name=email]').val(),
+        'subject': $('input[name=subject]').val(),
+        'message': $('textarea[name=message]').val()
+    };
+
+
+    $.ajax({
+        url: "mail.php",
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR)
+        {
+
+            $('#status').text(data.message);
+            if (data.code) //If mail was sent successfully, reset the form.
+                $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            $('#status').text(jqXHR);
+        }
+    });
+
+
+
+}
